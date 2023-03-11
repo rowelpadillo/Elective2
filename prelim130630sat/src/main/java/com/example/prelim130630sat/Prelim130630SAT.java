84945dff1a0db7fa8257c516e56a9a64558bb987
RJPadz <113223297+rowelpadillo@users.noreply.github.com>
2023-03-11 22:25:20 +0800

    Prelim

--- prelim130630sat/src/main/java/com/example/prelim130630sat/Prelim130630SAT.java
+++ prelim130630sat/src/main/java/com/example/prelim130630sat/Prelim130630SAT.java
@@ -3,12 +3,24 @@ package com.example.prelim130630sat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Prelim130630SAT extends AppCompatActivity {
+
+    Spinner spinner;
+    String[] robot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prelim130630_sat);
+
+        spinner = (Spinner) findViewById(R.id.spiner);
+        robot = getResources().getStringArray(R.array.Robots);
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item,robot);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
     }
 }
