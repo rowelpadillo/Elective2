9e9203fd9c4697c11b75887f90a87cc4bc173d88
rowelpadillo <113223297+rowelpadillo@users.noreply.github.com>
2023-03-18 17:40:53 +0800

    3/18/2023

--- actionbar2/src/main/java/com/example/actionbar2/MainActivity.java
+++ actionbar2/src/main/java/com/example/actionbar2/MainActivity.java
@@ -60,5 +60,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        AlertDialog dialog=builder.create();
+        dialog.show();
     }
 }
--- actionbar2/src/main/java/com/example/actionbar2/dialogFragment.java
+++ actionbar2/src/main/java/com/example/actionbar2/dialogFragment.java
@@ -1,2 +1,19 @@
-package com.example.actionbar2;public class dialogFragment {
+package com.example.actionbar2;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class dialogFragment  extends androidx.fragment.app.DialogFragment{
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_dialog,container,false);
+    }
 }
