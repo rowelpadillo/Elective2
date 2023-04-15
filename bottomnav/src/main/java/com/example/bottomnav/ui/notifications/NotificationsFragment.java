a306788236dab4d9e7a8b3111552e3242b5233ac
rowelpadillo <113223297+rowelpadillo@users.noreply.github.com>
2023-04-15 17:18:20 +0800

    4/15/23

--- bottomnav/src/main/java/com/example/bottomnav/ui/notifications/NotificationsFragment.java
+++ bottomnav/src/main/java/com/example/bottomnav/ui/notifications/NotificationsFragment.java
@@ -1,9 +1,12 @@
 package com.example.bottomnav.ui.notifications;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -24,8 +27,18 @@ public class NotificationsFragment extends Fragment {
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
+        textview = binding.textNotifications;
+        final Button checkStatus = binding.buttonCheck;
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        intentfilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+
+        checkStatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
         return root;
     }
 
